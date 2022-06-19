1bac212f9f06a808adb5f91035910f3a9f5f82c3
arifulprotik <mdarifulislam.protik@gmail.com>
2022-06-20 02:02:51 +0600

    Enabled CORS

--- pkg/server/run.go
+++ pkg/server/run.go
@@ -1,10 +1,18 @@
 package server
 
-import "github.com/labstack/echo/v4/middleware"
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/v4/middleware"
+)
 
 func (s *Server) Run() {
 	s.Echo.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: "method=${method}, uri=${uri}, status=${status}\n",
+	}))
+
+	s.Echo.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
 	}))
 	s.Echo.Logger.Fatal(s.Echo.Start(s.Cfg.Port))
 }
